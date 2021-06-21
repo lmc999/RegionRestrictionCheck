7f158063600e22161e0b65f6f2cdb6ea3c27f243
lmc999 <39726914+lmc999@users.noreply.github.com>
2021-06-21 12:13:41 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
 UA_Dalvik="Dalvik/2.1.0 (Linux; U; Android 9; ALP-AL00 Build/HUAWEIALP-AL00)";
+
 disneyauth="grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Atoken-exchange&latitude=0&longitude=0&platform=browser&subject_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiNDAzMjU0NS0yYmE2LTRiZGMtOGFlOS04ZWI3YTY2NzBjMTIiLCJhdWQiOiJ1cm46YmFtdGVjaDpzZXJ2aWNlOnRva2VuIiwibmJmIjoxNjIyNjM3OTE2LCJpc3MiOiJ1cm46YmFtdGVjaDpzZXJ2aWNlOmRldmljZSIsImV4cCI6MjQ4NjYzNzkxNiwiaWF0IjoxNjIyNjM3OTE2LCJqdGkiOiI0ZDUzMTIxMS0zMDJmLTQyNDctOWQ0ZC1lNDQ3MTFmMzNlZjkifQ.g-QUcXNzMJ8DwC9JqZbbkYUSKkB1p4JGW77OON5IwNUcTGTNRLyVIiR8mO6HFyShovsR38HRQGVa51b15iAmXg&subject_token_type=urn%3Abamtech%3Aparams%3Aoauth%3Atoken-type%3Adevice"
 disneyheader="authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"
 WOWOW_Cookie=$(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies | awk 'NR==3')
 TVer_Cookie="Accept: application/json;pk=BCpkADawqM3ZdH8iYjCnmIpuIRqzCn12gVrtpk_qOePK3J9B6h7MuqOw5T_qIqdzpLvuvb_hTvu7hs-7NsvXnPTYKd9Cgw7YiwI9kFfOOCDDEr20WDEYMjGiLptzWouXXdfE996WWM8myP3Z"
 Hulu_Content=$(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies | awk 'NR==5')
 Hulu_Cookie="guid=2B3BACF5B121715649E5D667D863612E; _h_csrf_id=457b2280eef19f28fbcb2992d1de37048e4b4097e257f63f155ed929b6e53687; bm_mi=28704523FCC474F1A9AC5F8483E541FC~8E5oFWEIhwYwpkQpyPS1kZ6CB0Zokl5veAxjYf7O4/x1xVYo4YOC8WLNMo0ZKz96fPzQ7MOUKsNPtE+0+7fQMEU3Lbln4h8G2WsunkEZDcPsvSSfhOuSJWpFs74B8i62Ge3i1F08W0PubOounXiPCmfgJPxGnDSKxUq8T5stR8eDfabmhwboVvt+Gmrc7RrvV63lXrJY4Fr8y9uMCJVKUnbOskbfObLcam251LJxD6T2vuSPOjKmkI9TJ93F/OvXMClMmA09MTHZ4GWm0vzeHQ==; ak_bmsc=E82D53D196AE742B2B7A5A73EEC0D850~000000000000000000000000000000~YAAQti43F5D7nRd6AQAApng/LAydmW2LypR2XE27prXF3huCKjeK4MKN+Wdy9P6qBFw4PbuszpyFsY2SazEUG5A6L9eHfB/FhO71fA0z8pn2xEVmrOkSnlw7HIKpsHvLxwXJAVaB1b2C1mPEsxJXtrvG0QKrUzf2gP0iu15YKdlCVGEHs5eoV/J11aBNpw2AUdZ1bW4zed/ix6VDRq5yDCej7O+r7CSV3qbuCwmR2Sw7jYK4X0BtXhF5PsiEHIjN+41O7TMV+ooB7dRY96Y0frDdViBgViq8di99yPGgbZr88a00flqwKMFz89hka02lgzESrQI6nlLxVD78oJfKmb9db4kRU9k3msYl1qhZWflKmaucRPV07eV6RWii27W9BKVr0/OaCzaMwQB/LHqCmRAY; __utma=155684772.684658975.1624241987.1624241987.1624241987.1; __utmc=155684772; __utmz=155684772.1624241987.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=155684772.1.10.1624241987; _ga=GA1.2.684658975.1624241987; _gid=GA1.2.138003965.1624241987; stc115168=tsa:1624241987497.203963452.98584127.9504090154979559.:20210621024947|env:1%7C20210722021947%7C20210621024947%7C1%7C1047148:20220621021947|uid:1624241987496.1200544265.72254.115168.1810957742.3:20220621021947|srchist:1047148%3A1%3A20210722021947:20220621021947; _gcl_au=1.1.2039255589.1624241988; _uetsid=84a93620d23211eb947f25585b965ddf; _uetvid=6c0fb310c8d711eba91e7f00bc7b8c6c; _scid=32c803fc-8be0-441f-885f-50f3d6406003; _sctr=1|1624204800000; _hulu_pgid=8396803; _hulu_uid=167194477; _hulu_e_id=iHI6sUwC0ZDJRQ218Yat7g; _hulu_bluekai_hashed_uid=0aaed4eecfceebd118abc89c11678718; _hulu_assignments=eyJ2MSI6W3siZSI6OTc5LCJ0IjoyNjk2LCJuIjoiaHVsdV8xMzU3OF93ZWxjb21lX2NvbnRyb2wifV19; _p_edit_token=JwysELYyqwYJ-otAhqGwxQ; _hulu_dt=g4rId3vFChPJIFepKv0l6yUq358-7WY9%2FWq2qIRKc8qfEUvlgQ--xQyvRy%2FhoqIoq832m7xXwwUwlG9YoRnJzjB2yKchSxZfMgjLnOm_En0lhJaOMnL1UcnvDVhHXxsrJv0FB_wCdnhBJeBbuJ6GUdtRcNuTwlamtrBLkN5xLvHh%2Fcl5C1O83l6xnDYcDIsp4vPRoyH%2F1UYcDwKKdL4EjTcaARazy9YuRzSeimJkkBhxAXgyYIQZjar3kVmnQ1lXhj35DyZOAFMJ_irXlAGL5lTsazF8qp7II7ctazxeWRZwWPXk_vqxObLThpf7sPyLrAOE6TvxZyj934NKaKN6gpmFvHq4GM94ly%2FK581sDdHwVueKTUcpJKe6vdy3RbZ8XY5JR4iR%2FSFNWWejrxoPoyyoB1GP_K8DWELg5KN56o8R3cEdcQXsWlxK4jVLXgCXlqty%2Fa6riovxGP2KsPLqW7E83tEraeY2tWaADbVoApsKL2NSnng2zPIxwE5yeNDJonR29Hc2ptZDuEXolmuYiAzUD2GnZl5G24OIWZXFBv4zP4PYxDGM2gFl%2FqM6TjGguGi_fUPXThT_rvrBD8WLpO94AufgzfJCbxC4vHKQ%2FwW6M3WeB1RCroVGz97LOxCUh%2FbGYlvYSBYFpyNfL3oIFqEmxqOIdieWEwzA4Ho0hfCqs%2FlhassrZqLXOa0DrGkAv1aXGL0VH9XCnAJG3mqb37tFK74h7S7H5L023yuxK4diqq5jIhXXnNlLqxSmGujmQTcehNTXfG_rF8voIGjekxx722MIRMU3fHqZ5zKX4tz8BL2PYdGh3x9TmzJibnCBIqo6i2lfP5BJB78zkspalr%2F9C1ai6QEIqHHmsQAm2NffZqEGu6PXwen5NGBIeePFSkAccoRNpZeAy7VCUKykCGdicu0jB3A-; XSRF-TOKEN=02c375da-1482-4c50-8a62-3e1905ff2432; _gat_hulu1=1; _hulu_pid=5080052296; _hulu_pname=joe%20biden%20america; _hulu_is_p_kids=0; _hulu_session=91UyDT0aYsIP6rKwG40b8EXz9DY-gb3WVQ5l_XqZSD2LBvW_%2Fw--TbLQ90QEZePwPSu9LQDNWF4Wu85wTrqcuJXpUPVO8OB7DaDnFtyBz_NpKxzVEnUACLbfMnEvDdIWCZ5SrmW_Fr08fo1OqmcYuPh5ZNEaCZI87wAk4mEoipDrK6vLhhNJDvTy0a7W7mb2dLMThOE3TSkbjQ2AVkd3YpJ2yt7YcduNBceuCUKk_YBl6a5Fn2FiONrZKwYzZXNW3IcA9h8RZni7zyLkBc6N49y%2FymqqF2nyjtLanmsf0zfLDxLHmw5SNLOwWZf_6ic5KZ8VgNrnx2moDVMSmU5Sn9rVWaImW1kjnQPNCwvhLJP0mddKMa0b%2FJdDqdzcrrUTnSSlrkAcRDUHeNbTd9LNbA66DUTlAnu4bd8F1jgjFwB6QEIbXsPaspkNgCQ2DEUgPbSf9SElwD4YLRNasvbXluR_lPSIKgdFzgktczaLCtpZOputtQR5dm6uekFjUmy40STPBq1aCG6AgqulStiGlgWwjo7xmpzdVQUrqWnmlhlnuYbYUxC3JTNzhNcfw5jpPz%2FFJJs8drNY1ByFJhNFghnLywoPVVpu2782fchQATWmH_YbKc4p8cEstwTlCsuGlkdrO3nf0RRvOCCmZPYQn40cWUx45BU9PbSKWAEt21%2FDPuqNw4MxwCkOXPXFOHCwDTu3aZeHZ4LJ9CsDwqTJdyn49N3G0oV6tIKAp%2FoUI504UDClLTghiKmLeTPq_6TKw7neebBPpuT7nOAmmGSu2O2DBmDyHUzFIaz2pX1q1cS1_PEowbs%2FUKM0uMlkzdiLQ3aBlJwknInqGfax%2FmSeh73phnKolwpSK_CDW4tKJ_zcE1hUrvaT_21zRhtpMpyCoxlqDFT6WTJpHhIt5L8tRHlSiyOiZqX9N2pr76ugnRIk6I1B2kDuafMprDvqkiDgNSWrld95LTJskxcZ2vNTi9LlHVyid82Af1lavt1IB8aBFyZbQMLl80fdjIp3mhCNfbAg0aYKkBifuNsv4WkFXiozBzIMWahG1I3mJTqjRlufKUkYUY_tZrzmsfq4kFZU2n2ufg0QxVfA8eEc914dgnnZPBuR8T0-; AMCVS_0A19F13A598372E90A495D62%40AdobeOrg=1; AMCV_0A19F13A598372E90A495D62%40AdobeOrg=-408604571%7CMCIDTS%7C18800%7CMCMID%7C57413330304479553574529114000647347829%7CMCAAMLH-1624846877%7C11%7CMCAAMB-1624846877%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCCIDH%7C-1857806863%7CMCOPTOUT-1624249277s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.6.0; _hulu_metrics_context_v1_=%7B%22cookie_session_guid%22%3A%2264a32034a29fb3e1868455e1d0c3d1e3%22%2C%22referrer_url%22%3A%22https%3A%2F%2Fwww.hulu.com%2Fhub%2Fhubs%2F%22%2C%22curr_page_uri%22%3A%22app%3Awatch%22%2C%22primary_ref_page_uri%22%3A%22urn%3Ahulu%3Ahub%3Ahome%22%2C%22secondary_ref_page_uri%22%3A%22www.hulu.com%2Fwelcome%22%2C%22curr_page_type%22%3A%22watch%22%2C%22primary_ref_page_type%22%3A%22home%22%2C%22secondary_ref_page_type%22%3A%22landing%22%2C%22secondary_ref_click%22%3Anull%2C%22primary_ref_click%22%3A%22Kick-Ass%22%2C%22primary_ref_collection%22%3A%22282%22%2C%22secondary_ref_collection%22%3Anull%2C%22primary_ref_collection_source%22%3A%22heimdall%22%2C%22secondary_ref_collection_source%22%3Anull%2C%22ref_collection_position%22%3A4%7D; _persisted_HEM=0e047fa12aee64bfb7087439257b48412d2c99284f661dd5597b947404c92c20; utag_main=v_id:017a2c5dde9a001fdb6dbc12936a03073001c06b00bd0$_sn:1$_ss:0$_st:1624243903349$ses_id:1624241987227%3Bexp-session$_pn:2%3Bexp-session$device_category:desktop%3Bexp-session$_prevpage:%2Fwatch%2F1ed6c0d7-2bc0-4b8a-8639-74fa7361a5d8%3Bexp-1624245703358$k_sync_ran:1%3Bexp-session$krux_sync_session:1624241987227%3Bexp-session$g_sync_ran:1%3Bexp-session$dc_visit:1$dc_event:1%3Bexp-session$dc_region:ap-east-1%3Bexp-session$hhid:9e25553e5dbba8f507f6b1c36a0ed17a741aa7cdd6ce03cb213d927fcdb22ffc%3Bexp-session; metrics_tracker_session_manager=%7B%22idle_time%22%3A1624242103392%2C%22session_seq%22%3A59%2C%22session_id%22%3A%222B3BACF5B121715649E5D667D863612E-49257fed-13fe-4a5a-b4ea-e545b82335dd%22%2C%22creation_time%22%3A1624241984744%2C%22visit_count%22%3A1%7D; bm_sv=3F0D41806D255DA2AB3280B681FA4630~kOV8lDWbANrCvHq0EeByqkCojQr9SUjssbFW8AXfPaquxDA4FNTGjzVCZ3tQ2Z5vQMuIR/dplyn1UeSVOlXA5mudUvuYb5PNCSVr8nmrkhqkosSY7ZSj92gU4YBGsJIqnvWHpGmmtIsjl/8gcNxWUw=="
+
 Font_Black="\033[30m";
 Font_Red="\033[31m";
 Font_Green="\033[32m";
@@ -721,7 +723,34 @@ function MediaUnlockTest_HuluUS(){
 	echo -n -e "\r Hulu:\t\t\t\t\t${Font_Red}Failed${Font_Suffix}\n"
 	return;		
 	
-}		
+}
+
+function MediaUnlockTest_encoreTVB() {
+    echo -n -e " encoreTVB:\t\t\t\t->\c";
+    tmpresult=$(curl -${1} ${ssll} -s --max-time 30 -H "Accept: application/json;pk=BCpkADawqM2Gpjj8SlY2mj4FgJJMfUpxTNtHWXOItY1PvamzxGstJbsgc-zFOHkCVcKeeOhPUd9MNHEGJoVy1By1Hrlh9rOXArC5M5MTcChJGU6maC8qhQ4Y8W-QYtvi8Nq34bUb9IOvoKBLeNF4D9Avskfe9rtMoEjj6ImXu_i4oIhYS0dx7x1AgHvtAaZFFhq3LBGtR-ZcsSqxNzVg-4PRUI9zcytQkk_YJXndNSfhVdmYmnxkgx1XXisGv1FG5GOmEK4jZ_Ih0riX5icFnHrgniADr4bA2G7TYh4OeGBrYLyFN_BDOvq3nFGrXVWrTLhaYyjxOr4rZqJPKK2ybmMsq466Ke1ZtE-wNQ" -H "Origin: https://www.encoretvb.com" "https://edge.api.brightcove.com/playback/v1/accounts/5324042807001/videos/6005570109001");
+    
+	if [[ "$tmpresult" == "curl"* ]];then
+        echo -n -e "\r encoreTVB:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
+        return;
+    fi
+	result=$(echo $tmpresult | python -m json.tool 2> /dev/null | grep 'error_subcode' | cut -f4 -d'"' )
+	if [[ "$result" == "CLIENT_GEO" ]]; then
+		echo -n -e "\r encoreTVB:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
+		return;
+	fi
+	
+	echo $tmpresult | python -m json.tool 2> /dev/null | grep 'account_id' > /dev/null 2>&1
+    if [ $? -eq 0 ];then
+		echo -n -e "\r encoreTVB:\t\t\t\t${Font_Green}Yes${Font_Suffix}\n"
+		return;
+	fi
+	
+	echo -n -e "\r encoreTVB:\t\t\t\t${Font_Red}Failed${Font_Suffix}\n"
+	return;
+
+}
+
+	
 
 function MediaUnlockTest() {
 	echo ""	
@@ -731,6 +760,7 @@ function MediaUnlockTest() {
 	MediaUnlockTest_HBOMax ${1};
 	MediaUnlockTest_SlingTV ${1};
 	MediaUnlockTest_PlutoTV ${1};
+	MediaUnlockTest_encoreTVB ${1};
 	MediaUnlockTest_BBC ${1};
 	MediaUnlockTest_ITVHUB ${1};
 	MediaUnlockTest_Channel4 ${1};
