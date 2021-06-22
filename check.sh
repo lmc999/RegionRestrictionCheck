e78b55926ea1d253bbd0f595e2b73dfa7ab2f297
lmc999 <39726914+lmc999@users.noreply.github.com>
2021-06-23 00:36:18 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -7,7 +7,7 @@ disneyheader="authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5Ni
 WOWOW_Cookie=$(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies | awk 'NR==3')
 TVer_Cookie="Accept: application/json;pk=BCpkADawqM3ZdH8iYjCnmIpuIRqzCn12gVrtpk_qOePK3J9B6h7MuqOw5T_qIqdzpLvuvb_hTvu7hs-7NsvXnPTYKd9Cgw7YiwI9kFfOOCDDEr20WDEYMjGiLptzWouXXdfE996WWM8myP3Z"
 Hulu_Content=$(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/cookies | awk 'NR==5')
-Hulu_Cookie="guid=2B3BACF5B121715649E5D667D863612E; _h_csrf_id=457b2280eef19f28fbcb2992d1de37048e4b4097e257f63f155ed929b6e53687; bm_mi=98A12BB1579032B9CA81C4F41FCC4D76~8E5oFWEIhwYwpkQpyPS1kX2U8y73BXeruxG9ASpoLZMdsQTjtyvfB1JhzX6EEHnxuU7yAvzvS/GGeb6ZjWEC8QZwI+q7HqHXsjXrYMBfn36Kk5ccNrl4B1YRqfvt5Ne/d5ihpUBHiwCMhr5edO7F4rApRIexCkrWrfDQ3XfKyM1UBumyKvHSsQuSjhTI6oQksn57PKxWOtzzHtgsPU4vJJ+X9Mu+9q86xiIwbdCPNsmyn8lPZHgAQz4gYe14IZ5e+VAG9Zz5JlVbr6HdFHoT4Q==; ak_bmsc=C2D69241DC5D57588DE0B838E9BE6AED~000000000000000000000000000000~YAAQffEoF6CLqhd6AQAAqZOHMQy4YgG0qd2B4Ad0YCo7mpExC/lcivnH22KbDsY2jOmj43C3KecGZZINVqaIxynHJ66Pvt9jWAi1pZ/v8Uu/hPeTYbw1JWamCd1vwOP0NDDb6CjL4Dd4wtJ5Y62l29fJGGH90cpQN75MEh4tt4OIEi0g0HR6Q5PnesgQ/V+ZrgaX2kNRyxw/Yj+BNvqapCY646d0toe9LZucyLM0qXhwzbnbVP/9aY7NAcGJOUFCBx91FT0gBKY6V9X2tCuvgr10A5FilTSAqfc/7nCIBaFoIjSwHZH7JVQfeyzIkbKBUd6X5SL0BYo9CaM4B3TMYNbyCE4N3rRgWDJVkeeE4t3PX3ZvfTvg+lgnhsoa88GDl7mKzq6qPQTfkuYb77Lm74/Q; __utma=155684772.15046934.1624328687.1624328687.1624328687.1; __utmc=155684772; __utmz=155684772.1624328687.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=155684772.1.10.1624328687; _ga=GA1.2.15046934.1624328687; _gid=GA1.2.898989183.1624328688; _uetsid=bbdaffc0d30011ebae3b83ed1d284d15; _uetvid=6c0fb310c8d711eba91e7f00bc7b8c6c; _gcl_au=1.1.1445813666.1624328688; stc115168=tsa:1624328688867.701667577.3067198.6769165591537141.1:20210622025448|env:1%7C20210723022448%7C20210622025448%7C1%7C1047148:20220622022448|uid:1624328688866.27138477.980460644.115168.64013158.5:20220622022448|srchist:1047148%3A1%3A20210723022448:20220622022448; _scid=cbb3a569-173c-46dc-ad94-1217690c0480; _sctr=1|1624291200000; _hulu_pgid=119005187; _hulu_uid=27700158; _hulu_e_id=t7hpWpSXllhMLLFtQgwj6Q; _hulu_bluekai_hashed_uid=b01118dc5ca5a6c3ff2d52da58521763; _hulu_assignments=eyJ2MSI6W3siZSI6OTc3LCJ0IjoyNjkzLCJuIjoiaHVsdV8xMzU4M19saXZldHZfbmV0d29ya3NfY3RhIn1dfQ%3D%3D; _p_edit_token=QG15y0eX1JM8Rca0ipSwbg; _hulu_dt=_NfpcTpR6vmlL3iaiWUHjQSkK5c-8tLIXduBD%2F1lt6ABDRyCLg--ZajrNgYeVcAdpLYdy1UcOCku%2FakNRRxTuAo%2FM76B4ZKTRipMZame4CRUQHtpG5CquWpxC231nPBtkFC5k3lh%2Frp4MR8dcXyXkXiTqGf89EsmoK3FZtO_NTY72%2FG%2Fzm_sDyoMbFHTlMFd2z2Yx2Hx0UejA6kzbc_cwNH%2FccXBVVXm93_nMSTpN9VNxUpFJ2mk_p%2FvtWShFFvfTeXj_mt1DV8shzxLXdMELXpw2Dsa66YkhwEiUnuVvX2xqZXfnIcpmw6hS2Pbe_wDA%2FATJBNae94_qOC%2FblP_f_BaoJ17kubZ3BOB_vnsN9FVg2_b49mUj1Xl5VwnhBLKj2dNADHwBJmU7fqQ4GFVia92Rc%2FxvdZ6CMQCiBWp5B%2F6qVQFhsE3HEyPVqGiaVGofNLdUz%2Fz7y28ZSvfeMsq%2Fx63d5oRBcaOehRpiS3W2BObYFTjpBlWgU5wmYofOrT9oxZn9FI9in1Z9swfYcAFSGWXZZIzcUaUPwW7PI64zOEcASdVXL9al3VGkEMFzBbu1J3%2FhaWFaL2ZAdIQlYeTX64HYduiBtGYF3wVTx7DVxsJdIEve2T%2Ft5EGo11Ho02zbRQGFZlwWAozpeXv4iHCFcNu23nzebNvE1rKMOF2ejx%2Fe2U8K23vbrpxfhjglivVhH_9fhWjJYbaLCvjQXxkUYM_qZgINJEIQ_dUUh5dKEHOpK4YS%2F3Zk%2F5pngd13CC39hh7JsJcnF5P%2Fy3bsbi8YdkALtax72ExQ5jKNjhWowx_bTL_jquaJSi%2Fo4woznfPJCqeTixOkrPKnr4sxyUinFMvU2U5hMxT1dldkg3g4CZcOFETgs1lDpYJl0pFRW3QnV1BqZErZicVWQz7Zu9RL3EdBzXYdZs-; _persisted_HEM=1c7283eff64c1af675275a5257f759263507e69cfcfc4cfda46566d19d771fbc; _gat_hulu1=1; XSRF-TOKEN=c50fde0e-13af-41aa-9227-58b84c7f7ad3; geo=23.1192247%26113.2199658%261624328812537; _hulu_session=chkaxXQj_Wgz77zpprc0dqaAc8k-dS6p06DtW0oeFDNsl4LlaQ--njaMgp6HPG6CpHGtN2zJeaGM6lzNHBYbUp%2FImohbo_0ceuuaYwRKW4sfYPGRWIv7OaV5NzFO4Kj_Ryc35OBM6ASnpt6L_S5AFFJhfUNgBanXiby%2FqowtGDDtKm97jSX2EJmgXWBXeczD5T7MbY6Uq56ExELiEHLZRfBnLic%2Fc2CKmTgXfW%2F1NCZ7YcPQSaJ6ek4IwolV3mOEpC_pK6IPUW4kVDHaspg6j4S4dIbyAhts53ZQOmoJChAZqR2vep8ajJMSdukGdDYBmV1h9%2Fsz2Z_hXeSKHlDmn_fyBhwiwKIB9t_CMVqsydkgNUGwqQMUk3cTY7a3XKVaoMPtjUj93RL%2FYdVG6_q_S710kv1UA14K3InJ9hCf2DIf61UrPm8e4ODw7f7crsEKftYrEURIL314tzHj_BIv_o8zU5jAX1zjuePh2jUVMmKRTK5YMkn9azDdCECk94MDK2g%2F6364MUw4Qr4Ef1mUaSZm1t%2FOTo%2Fp8x_A_QJcHDVibrHs%2Fnagp%2FzjSAMLKDhvV7xHP0r1MFCg5F3h8qlElhH2OW_hu7unuLcMDeqIQJBSJwDHXUwauB_enFn2rL0VR2DKbEBB0LnndRsus4WbccMjGpn72J5uO8Ygv7uC8jog1P3VGo%2F78lBcX1tXb4kpUiAU8CCfAn_OnIyH7SsgskEXv7HYC9bl7%2FZ3RJdsQ8oK7HRX1HyVfLvV7vX0ez9wLT0pnlpGQRHMSHI6d90Isc0E2ayFHThWNSC4H3RaU6DqjkNTSzLEMwakLxF1iyZxwEdrUSoTBqpBSPlb%2Fu5bk74wgtqAdbBWdpEsWTpSN_MNGwYb4IeMY70URvFF9GMPuCB45bptcC1DUS1U%2F_IPiWhXcxNKm9G14nKzofA18A1kusG65ufvgpHaGD%2Fw_hGnorZV_gie4GDWYNAp9LTkweQ5eRhmYCPIX8gRprzDv1yEYo__sIi%2FygPI4KWagtafxTmAuXKKTTLkLplq_lLp2dy4od5PhcCR7NxJxovjzc91IYqJBltjw2JSToLHweKGeGm8%2FqgFLzHkwSKXRcpPTbQf5EQAMEc-; _hulu_pid=27700158; _hulu_pname=Main; _hulu_is_p_kids=0; bm_sv=9A544DD97C5AC8609E896D599E7CCB54~kOV8lDWbANrCvHq0EeByqil2DGdG+32wj7h4iKbbCi2S2OKABBaakQGYNWf92LQj3ve/A1Y1kzP0s5DiQPGBzqheVtUTUa1NLVlXN9KjRSpl+uhSkUnEF2zq3P8TImPQTeHmG996TDm9rRWzTlsXF2zBiO1iksOjLnpUC/7vW90=; AMCVS_0A19F13A598372E90A495D62%40AdobeOrg=1; AMCV_0A19F13A598372E90A495D62%40AdobeOrg=-408604571%7CMCIDTS%7C18801%7CMCMID%7C57413330304479553574529114000647347829%7CMCAAMLH-1624933633%7C11%7CMCAAMB-1624933633%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCCIDH%7C-1857806863%7CMCOPTOUT-1624336033s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C4.6.0; _hulu_metrics_context_v1_=%7B%22cookie_session_guid%22%3A%22c3718e4141e5313dfcafe08d9dbf03fe%22%2C%22referrer_url%22%3A%22https%3A%2F%2Fwww.hulu.com%2Fhub%2Fhome%22%2C%22curr_page_uri%22%3A%22app%3Awatch%22%2C%22primary_ref_page_uri%22%3A%22urn%3Ahulu%3Ahub%3Ahome%22%2C%22secondary_ref_page_uri%22%3A%22www.hulu.com%2Fwelcome%22%2C%22curr_page_type%22%3A%22watch%22%2C%22primary_ref_page_type%22%3A%22home%22%2C%22secondary_ref_page_type%22%3A%22landing%22%2C%22secondary_ref_click%22%3Anull%2C%22primary_ref_click%22%3A%22High%20School%20DxD%22%2C%22primary_ref_collection%22%3A%22282%22%2C%22secondary_ref_collection%22%3Anull%2C%22primary_ref_collection_source%22%3A%22heimdall%22%2C%22secondary_ref_collection_source%22%3Anull%2C%22ref_collection_position%22%3A3%7D; utag_main=v_id:017a3188cbb100021414281ab47e03073001c06b00bd0$_sn:1$_ss:0$_st:1624330646379$ses_id:1624328686514%3Bexp-session$_pn:4%3Bexp-session$device_category:desktop%3Bexp-session$_prevpage:%2Fwatch%2Fad8d966d-82be-4bfd-99fd-f19a724b404a%3Bexp-1624332446388$k_sync_ran:1%3Bexp-session$krux_sync_session:1624328686514%3Bexp-session$g_sync_ran:1%3Bexp-session$dc_visit:1$dc_event:1%3Bexp-session$dc_region:ap-east-1%3Bexp-session$hhid:1da4f9109b4714b9cf4206d9cb9d4bb041024a0b5d2d08230e95b67cfabd8523%3Bexp-session; metrics_tracker_session_manager=%7B%22idle_time%22%3A1624328846431%2C%22session_seq%22%3A46%2C%22session_id%22%3A%222B3BACF5B121715649E5D667D863612E-770a97c3-aea5-44df-a21d-4d47ceb4ce52%22%2C%22creation_time%22%3A1624328684490%2C%22visit_count%22%3A1%7D"
+Hulu_Cookie="_hulu_assignments=eyJ2MSI6W119; bm_mi=35D6F5FDB843EBDBB36F2563EA2F905E~mduMYozDcKPxgnTnPbzGFdcOP7sL5laOvrw84v4azoN9hvFV/DVFJN+l5W4y1J0bThCyGjkKxzcmJMdwkhJnwQUsxQQvgoMRNvcER2ewPlvT/3bmwC+auTWnyDkPIIMpDKHNtBMXP+oyVHwZPqLmOmQoPmpZFA8roNv9r/Jveo+ib+lkx3li9clr7lbSmlyePLwuV5mgsNbVR6j8ySH99U42Mqaym3u4oX/Az+5viuECqFnU9GjZ5vyG4HueFdvf/BmVfvWGJqlfdCvJIb+U9A==; _h_csrf_id=aa68dcc9ba46d7cec1855834beb2b6ac2d67bf9fb321e6b5c8e9da75b4fb91ff; ak_bmsc=5F4C29E6C5485141C7E6B1148B6064E7~000000000000000000000000000000~YAAQF84zuIqZ6ep5AQAA7y4cNAz08B6KWT5/8gfBXXngCOxzgadCnkxuJ29Q3LiXizDsKmOB8p934pKVKGkL2wR7bGWH3erDn+2bPhlhenwPNr+eG42SIEQ47iieXR4Oaej2nUzgtlj9HXVGdDJyESv3gOI6Xcu1Svzq4ilosGur2NcgzmWX/ulrhWlTWROD06otoWBk+/2r14XHZgTguBHTriJSyP+8MvtkJM3m/ho332McuA8ldX+QLfFrT/JcUtXzOpjM6/FhYFOr4CmV5s1ILDKfInOmvlkV9YBNRHEpAEuNV3lBP4nuUvmlBYl1pywWakotbdGzFV+UziHf7uJgTuoTU9nHUTM9rewlaxecad0oIyJHYdRDL4oUBeek2L9RL2sZLxXE2iVqnyL3HXDMem4gq+1PNQ+EUFLi9mY=; _hulu_pgid=8396803; _hulu_uid=168956133; _hulu_e_id=CR7dX_MmCn_5Kf-m_PKR4g; _hulu_bluekai_hashed_uid=77d8272a48433b34232a5e018423d810; _p_edit_token=Fg4o-Wx5CztBgorxmtv3SQ; _hulu_dt=3qZnlwE6aqYevqV4EyyjU7HOcJ8-6h_O%2FeEQnz8lu2hylIv5Gg--H%2Ft7tif1%2FRYPgAiy_WuOpoK5_yjFMP3DVNhI%2FosTJcX6IpcpLvxcczINNGo1YUYYH2_P3Z3Y73TOnzogcP7y0uXHGLEfC_Yp6lqXa0M_j0VnE%2Fbm1OHSDiRRi9p3YiOl4zw0xeeBDO4iUPFhqT1OTKA4npSkU00aGWMj2KIugmSJCK8hWEWQfbYSRkat1maUuM8WJ70t%2F1PHoVuK30Y9K5CxdZMlhbxP49N4n196Wa4j%2FWDIMJ7gsgkEhPVF5tvV5xLZ56%2Fa%2FzDOrHh1xZLngL_Q2DJt36CCSa%2FpyWNMrO3Hi9oO0U1w7yZT8Q95Q9TEUEVxd2llJQg8eRyKIXdQL7XizNer6BX0IZ84hXB2dzwn3KVVp49cOgBMl3AsZeQplK1YeCJGM1a0HoFtYqGxH3xzjvOUGAdg5W3frMkQedRwVYpysGqZGBOsFQ0Cl_sZGlmk5UEofBh%2Fsr9YOoH5KOyFT0R6Iq7n0cF1cCnkV_aA8al7a3ahuCQc48NFKqh5v9DkajJdBbH82VK851%2F0Hj3sc9s54LN98ofQZWnvln%2FMcooXfqQPUr8maUTwYykYqIpnlSpeUVkUgYnQZmu4QKmhoZ_R5pamd0W0mT8MZLNxBYm8ShjS%2FZciaSU1hI9%2F4FJu1dgEDOfquvG6XROPrw9QPIIpyPuhXsc0bz2VPHEJ9rgcSwSnIslsW_aFZOfD_7tA6P88YmDxDRWcmfCWPyT108%2FJPbvrkOAEG_v%2FXIQWKTMCzYJrk5tty7MfYT8khkY5YuJwfvjstiTRYtuQUjlu3l_yErmfJnEyNYtlTPqpH7jN3H6OFsWtZUVMNN3l6TX4imKSZInzCBwxMWI5qRdbUTEGXHBAonE08GALMfk-; XSRF-TOKEN=a24fd2ec-8632-49b1-a998-4785a8082804; _hulu_session=0i7960bmMkeCewflAevUkVrv1Zg-hTnsUnywh03JxBkx0diffw--4Fg3LER0t0f211ZFa%2F8EG0IVK3REieWnY4_neDyxsS1qQU_lJv8fWfPQurddXDEvIxiFJN698lsfYDkJeP8I9lz17zrzbbYXOjYcjF_%2FG0Yd8yC_ThifCKXP8fgzesXEukW25VoXr83JMa59NwMK9F7o_B92SU8kNBWftLrWSFoy9B9GMEf40R5gFixS9yUgItvMHmEq6JP6N2cOZhl9MDMETmsOYCA3kW9OvUqcrTlu3daklnpksIi%2Fwu3f7HMTc%2FqCkHptW_Q6R98mC%2F68A6WZVhC9lQmvgfhQ%2FLbFkkkokqMxhoRzO_5JckaLlFHsF1udOFTu17BlUrYGsAZx3SvPfnDVhsaeyu07SmRtr0yEASsDqdI5Zsy1ov_qqzb23LJcKDVHjoYXHbLhty2GX3fL6q92xtfVXfwmo9V%2FsY1i6rhaBEVrdZcamFsufYt8qKHe6CklB8aX_Yvon34RFPS7BV0S4H%2FqENXDGhBOFun2yLShlDFU3b7wFqVpnqIF%2FxS85xa2ml75aohHOFmMADKrpHJnekK57qqi0ZxBNJs4t7_QLRCfU6pzg9ohmj_2bJ1oCWD9xA0pUV5CT15UWsS4%2FgYzxr821QQuHo2krBkAJaGVxXNVFMx4%2FAgjIat_swZZfanojuxgBVoQyCt06JFZmG1eCk4PgkK%2Fl6Q1y347haNQVA49O0Qa1aQvCQtxF5lSmjT_8Fv6d_ToiuM6DzJYEovonvWMN_AiKH3olIHJgeh5csClKjyz7MHGPy91vroU%2FWjnRfIT2m5YI2SUrBbnUQYXsOVATRL%2Fda98BA29F7H3C5YCVBvEg6jPfXSLSR1y509GoyAFIOTIHixBVbyl8I1tbILNmr3NiYnnbpRvfylATsz_zy3kJZ_01HfB0WE5V%2F0lz9_2tOqzcK8CW5v%2FxD3o%2F75FA%2F6lFK2QT8E-; _hulu_pid=168956133; _hulu_pname=Hayden; _hulu_is_p_kids=0; guid=81F1FEC9945F68CD681F45315ADB0096; AMCVS_0A19F13A598372E90A495D62%40AdobeOrg=1; AMCV_0A19F13A598372E90A495D62%40AdobeOrg=-408604571%7CMCIDTS%7C18801%7CMCMID%7C41103232120510992910533052126393276069%7CMCAID%7CNONE%7CMCOPTOUT-1624379254s%7CNONE%7CvVersion%7C4.6.0; _hulu_metrics_context_v1_=%7B%22cookie_session_guid%22%3A%22445360c897752737afb5c6a53548f3a7%22%2C%22referrer_url%22%3A%22%22%2C%22curr_page_uri%22%3A%22app%3Awatch%22%2C%22primary_ref_page_uri%22%3A%22urn%3Ahulu%3Ahub%3Ahome%22%2C%22secondary_ref_page_uri%22%3A%22www.hulu.com%2Fwelcome%22%2C%22curr_page_type%22%3A%22watch%22%2C%22primary_ref_page_type%22%3A%22home%22%2C%22secondary_ref_page_type%22%3A%22landing%22%2C%22secondary_ref_click%22%3Anull%2C%22primary_ref_click%22%3A%22The%20Future%20Diary%22%2C%22primary_ref_collection%22%3A%22282%22%2C%22secondary_ref_collection%22%3Anull%2C%22primary_ref_collection_source%22%3A%22heimdall%22%2C%22secondary_ref_collection_source%22%3Anull%2C%22ref_collection_position%22%3A0%7D; metrics_tracker_session_manager=%7B%22session_id%22%3A%2281F1FEC9945F68CD681F45315ADB0096-3ce0d9b1-fed1-4da7-907f-2b4e715c3c82%22%2C%22creation_time%22%3A1624371865113%2C%22visit_count%22%3A1%2C%22session_seq%22%3A36%2C%22idle_time%22%3A1624372056067%7D; bm_sv=78952BDC542C106C5EF42E1FB31E33F4~s4ubemiuyh97XfkOxWkvvouJJFqFbnNLNUDvA8lw5npJJr8J+KeOM5fLCjgrrnZOTqo0sYedZtDYrPIz067XEP1QDQ3TTvrg4PxZ2SmTUPrY5ydoJJIUIsvaGzK+89tqZwLC9LZQa5wueZClFqVDlA=="
 
 Font_Black="\033[30m";
 Font_Red="\033[31m";
@@ -40,16 +40,6 @@ if [ -n "$local_ipv4" ]
 	else
 		local_isp=$(curl -s -6 https://api.ip.sb/geoip/${local_ipv6} | cut -f1 -d"," | cut -f4 -d '"')
 fi		
-
-clear;
-echo -e "流媒体平台及游戏区域限制测试";
-echo ""
-echo -e "${Font_Green}项目地址${Font_Suffix} ${Font_Yellow}https://github.com/lmc999/RegionRestrictionCheck ${Font_Suffix}";
-echo -e "${Font_Green}BUG反馈或使用交流可加TG群组${Font_Suffix} ${Font_Yellow}https://t.me/gameaccelerate ${Font_Suffix}";
-echo ""
-echo -e " ** 测试时间: $(date)";
-echo ""
-
 
 
 function GameTest_Steam(){
@@ -827,21 +817,28 @@ function MediaUnlockTest_Viu.com() {
 }
 
 
-function MediaUnlockTest() {
-	echo ""	
-	echo "=============欧美地区解锁============= "
+function US_UnlockTest() {
+	echo "=============美国地区解锁============="
 	MediaUnlockTest_HuluUS ${1};
 	MediaUnlockTest_HBONow ${1};
 	MediaUnlockTest_HBOMax ${1};
 	MediaUnlockTest_SlingTV ${1};
 	MediaUnlockTest_PlutoTV ${1};
 	MediaUnlockTest_encoreTVB ${1};
+	echo "======================================="
+}	
+
+function EU_UnlockTest() {
+	echo "=============欧洲地区解锁============="
 	MediaUnlockTest_BBC ${1};
 	MediaUnlockTest_ITVHUB ${1};
 	MediaUnlockTest_Channel4 ${1};
 	#MediaUnlockTest_Molotov ${1};
 	echo "======================================="
-	echo "============大中华地区解锁============= "
+}	
+	
+function HKTW_UnlockTest() {	
+	echo "=============港台地区解锁============="
 	MediaUnlockTest_MyTVSuper ${1};
 	MediaUnlockTest_NowE ${1};
 	MediaUnlockTest_ViuTV ${1};
@@ -849,11 +846,13 @@ function MediaUnlockTest() {
 	MediaUnlockTest_HamiVideo ${1};
 	MediaUnlockTest_LineTV.TW ${1};
 	MediaUnlockTest_BahamutAnime ${1};
-	MediaUnlockTest_BilibiliChinaMainland ${1};
 	MediaUnlockTest_BilibiliHKMCTW ${1};
 	MediaUnlockTest_BilibiliTW ${1};
 	echo "======================================="
-	echo "============日本地区解锁=============== "	
+}
+	
+function JP_UnlockTest() {	
+	echo "============日本地区解锁============"	
 	MediaUnlockTest_AbemaTV_IPTest ${1};
 	MediaUnlockTest_Paravi ${1};
 	MediaUnlockTest_unext ${1};
@@ -864,7 +863,11 @@ function MediaUnlockTest() {
 	MediaUnlockTest_UMAJP ${1};
 	MediaUnlockTest_Kancolle ${1};
 	echo "======================================="
-	echo "============全球性平台解锁============= "	
+}	
+
+function Global_UnlockTest() {		
+	echo ""		
+	echo "============跨国平台解锁============"	
 	MediaUnlockTest_Dazn ${1};
 	MediaUnlockTest_Netflix ${1};
 	MediaUnlockTest_DisneyPlus ${1};
@@ -875,32 +878,164 @@ function MediaUnlockTest() {
 	echo "======================================="	
 }
 
-curl -V > /dev/null 2>&1;
-if [ $? -ne 0 ];then
-    echo -e "${Font_Red}Please install curl${Font_Suffix}";
-    exit;
-fi
+function CheckV4() {
+	echo -e " ${Font_SkyBlue}** 正在测试IPv4解锁情况${Font_Suffix} "
+	echo "--------------------------------"
+	echo -e " ${Font_SkyBlue}** 您的网络为: ${local_isp}${Font_Suffix} "
+	check4=`ping 1.1.1.1 -c 1 2>&1`;
+	if [[ "$check4" != *"unreachable"* ]] && [[ "$check4" != *"Unreachable"* ]];then
+		isv4=1
+	else
+		echo -e "${Font_SkyBlue}当前主机不支持IPv4,跳过...${Font_Suffix}"
+		isv4=0
+	fi
 
+	echo ""
+}
 
-echo -e " ${Font_SkyBlue}** 正在测试IPv4解锁情况${Font_Suffix} "
-echo "--------------------------------"
-echo -e " ${Font_SkyBlue}** 您的网络为: ${local_isp}${Font_Suffix} "
-check4=`ping 1.1.1.1 -c 1 2>&1`;
-if [[ "$check4" != *"unreachable"* ]] && [[ "$check4" != *"Unreachable"* ]];then
-    MediaUnlockTest 4;
-else
-    echo -e "${Font_SkyBlue}当前主机不支持IPv4,跳过...${Font_Suffix}"
-fi
-
-echo ""
-
+function CheckV6() {
 check6=`ping6 240c::6666 -c 3 -w 3 2>&1`;
 if [[ "$check6" != *"unreachable"* ]] && [[ "$check6" != *"Unreachable"* ]];then
+	echo ""
+	echo ""
 	echo -e " ${Font_SkyBlue}** 正在测试IPv6解锁情况${Font_Suffix} "
+	echo "--------------------------------"
 	echo -e " ${Font_SkyBlue}** 您的网络为: ${local_isp}${Font_Suffix} "
-    MediaUnlockTest 6;
+    isv6=1
 else
     echo -e "${Font_SkyBlue}当前主机不支持IPv6,跳过...${Font_Suffix}"
+	ipv6=0
 fi
 echo -e "";
+}
+
+function Goodbye(){
 echo -e "${Font_Green}本次测试已结束，感谢使用此脚本 ${Font_Suffix}";
+}
+
+clear;
+
+function ScriptTitle(){
+echo -e "流媒体平台及游戏区域限制测试";
+echo ""
+echo -e "${Font_Green}项目地址${Font_Suffix} ${Font_Yellow}https://github.com/lmc999/RegionRestrictionCheck ${Font_Suffix}";
+echo -e "${Font_Green}BUG反馈或使用交流可加TG群组${Font_Suffix} ${Font_Yellow}https://t.me/gameaccelerate ${Font_Suffix}";
+echo ""
+echo -e " ** 测试时间: $(date)";
+echo ""
+}
+ScriptTitle
+
+function Start(){
+echo -e "${Font_Blue}请选择检测项目，直接按回车将进行全区域检测${Font_Suffix}"
+echo -e "${Font_SkyBlue}输入数字【1】：【跨国平台+美国平台】检测${Font_Suffix}"
+echo -e "${Font_SkyBlue}输入数字【2】：【跨国平台+日本平台】检测${Font_Suffix}"
+echo -e "${Font_SkyBlue}输入数字【3】：【跨国平台+港台平台】检测${Font_Suffix}"
+echo -e "${Font_SkyBlue}输入数字【4】：【跨国平台+欧洲平台】检测${Font_Suffix}"
+echo -e "${Font_SkyBlue}输入数字【5】：【  只进行跨国平台 】检测${Font_Suffix}"
+read -p "请输入正确数字或直接按回车:" num
+}
+Start
+
+function RunScript(){
+	if [[ -n "${num}" ]]; then
+		if [[ "$num" -eq 1 ]]; then
+			clear
+			ScriptTitle
+			CheckV4
+			if [[ "$isv4" -eq 1 ]];then
+				Global_UnlockTest 4
+				US_UnlockTest 4
+			fi
+			CheckV6
+			if 	[[ "$isv6" -eq 1 ]];then
+				Global_UnlockTest 6
+				US_UnlockTest 6
+			fi	
+			Goodbye
+			
+		elif [[ "$num" -eq 2 ]]; then
+			clear
+			ScriptTitle
+			CheckV4
+			if [[ "$isv4" -eq 1 ]];then
+				Global_UnlockTest 4
+				JP_UnlockTest 4
+			fi
+			CheckV6
+			if 	[[ "$isv6" -eq 1 ]];then
+				Global_UnlockTest 6
+				JP_UnlockTest 6
+			fi	
+			Goodbye
+			
+		elif [[ "$num" -eq 3 ]]; then
+			clear
+			ScriptTitle
+			CheckV4
+			if [[ "$isv4" -eq 1 ]];then
+				Global_UnlockTest 4
+				HKTW_UnlockTest 4
+			fi
+			CheckV6
+			if 	[[ "$isv6" -eq 1 ]];then
+				Global_UnlockTest 6
+				HKTW_UnlockTest 6
+			fi	
+			Goodbye
+			
+		elif [[ "$num" -eq 4 ]]; then
+			clear
+			ScriptTitle
+			CheckV4
+			if [[ "$isv4" -eq 1 ]];then
+				Global_UnlockTest 4
+				EU_UnlockTest 4
+			fi
+			CheckV6
+			if 	[[ "$isv6" -eq 1 ]];then
+				Global_UnlockTest 6
+				EU_UnlockTest 6
+			fi	
+			Goodbye
+			
+		elif [[ "$num" -eq 5 ]]; then
+			clear
+			ScriptTitle
+			CheckV4
+			if [[ "$isv4" -eq 1 ]];then
+				Global_UnlockTest 4
+			fi
+			CheckV6
+			if 	[[ "$isv6" -eq 1 ]];then
+				Global_UnlockTest 6
+			fi	
+			Goodbye
+				
+		else
+			echo -e "${Font_Red}请重新执行脚本并输入正确号码${Font_Suffix}"
+			return
+		fi
+	else
+		clear
+		ScriptTitle
+		CheckV4
+		if [[ "$isv4" -eq 1 ]];then
+			Global_UnlockTest 4
+			US_UnlockTest 4	
+			JP_UnlockTest 4
+			HKTW_UnlockTest 4
+			EU_UnlockTest 4
+		fi	
+		CheckV6
+		if [[ "$isv6" -eq 1 ]];then
+			Global_UnlockTest 6
+			US_UnlockTest 6	
+			JP_UnlockTest 6
+			HKTW_UnlockTest 6
+			EU_UnlockTest 6	
+		fi
+		Goodbye	
+	fi
+}	
+RunScript	
