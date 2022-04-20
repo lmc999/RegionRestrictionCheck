69dd3df7c285f5981ce41f7cf85a66f5ef8b3106
lmc999 <39726914+lmc999@users.noreply.github.com>
2022-04-20 10:47:44 +0800

    update promo content

--- check.sh
+++ check.sh
@@ -2967,7 +2967,7 @@ function Goodbye() {
 		echo -e "${Font_SkyBlue}Lite系列优惠码(Tiny除外): ${Font_Suffix}"
 		echo -e "${Font_Green}年付七折【Lite-Annually-Recur-30OFF】 ${Font_Suffix}"
 		echo -e "${Font_Green}半年付八折【Lite-Semi-Annually-Recur-20OFF】 ${Font_Suffix}"
-		echo -e "${Font_Red}传送门：${Font_Suffix}https://www.dmit.io/aff.php?aff=3198"
+		echo -e "${Font_Red}传送门：${Font_Suffix}https://www.dmit.io/aff.php?aff=3198&gid=1"
 		echo -e "================================================"
 		echo -e ""
 		echo -e ""
