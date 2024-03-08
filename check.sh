3b6809dc078a93fe1b5020b9703764cae0f8a5ca
lmc999 <39726914+lmc999@users.noreply.github.com>
2024-03-08 17:22:17 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -3854,7 +3854,6 @@ function Goodbye() {
         echo -e "${Font_Yellow}检测脚本当天运行次数: ${TodayRunTimes}; 共计运行次数: ${TotalRunTimes} ${Font_Suffix}"
         echo -e ""
         #bash <(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/reference/AD/AD${ADN})
-        bash <(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/reference/AD/ADDV)
 	echo -e ""
         bash <(curl -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/reference/AD/AD2)
     fi
